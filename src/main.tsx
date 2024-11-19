9a2738d2413be65c7c63235ae6bc8e6989d087eb
Kiefer <kiefhay642@outlook.com>
2024-11-19 13:50:38 -0800

    Changed from next -> react

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import Navbar from "./components/Navbar.tsx";
 import Header from "./components/Header.tsx";
 import SocialButtons from "./components/SocialButtons.tsx";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
