7145b0ef1308dae7f1fb7ffdafc1a8cbfb4600d5
Kiefer <kiefhay642@outlook.com>
2024-11-19 13:41:19 -0800

    Added Speed Tester via Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import Navbar from "./components/Navbar.tsx";
 import Header from "./components/Header.tsx";
 import SocialButtons from "./components/SocialButtons.tsx";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
