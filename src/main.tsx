c794714a487863218cb2f86f15f15366491e359a
Kiefer <kiefhay642@outlook.com>
2024-11-19 11:59:06 -0800

    Testing change

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Navbar from "./components/Navbar.tsx";
 import Header from "./components/Header.tsx";
-import Timeline from "./components/Timeline.tsx";
 import SocialButtons from "./components/SocialButtons.tsx";
 
 createRoot(document.getElementById('root')!).render(
@@ -11,8 +10,5 @@ createRoot(document.getElementById('root')!).render(
         <Navbar />
         <Header />
         <SocialButtons />
-        <main className="container px-6 sm:px-8 w-1/2">
-            <Timeline />
-        </main>
-    </StrictMode>,
+    </StrictMode >,
 )
